8dbc6bff8d19c7e612820d29cdd1d8b70048400f
yusuplusminus <yoesoep17@gmail.com>
2023-06-26 17:34:46 +0700

    first check point

---
+++ App.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+// });
+
+export default App;
